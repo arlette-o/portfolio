7690aadfeeea207e051f95f1d056763d3ec99e8a
Arlette Olalde <aolaldeflores@gmail.com>
2025-01-31 13:31:44 -0800

    Added Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import Navbar from "./components/Navbar.tsx";
@@ -23,6 +24,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
         </Routes>
         <Footer />
+        <Analytics />
       </ThemeProvider>
     </Router>
   );
